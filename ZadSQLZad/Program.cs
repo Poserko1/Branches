c8d4eb750cbe3bd6707997d5b39c321765eed81b
poserko <kokolin2204@gmail.com>
2023-03-13 09:12:52 +0200

    ZadSQL2

---
+++ ZadSQLZad/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ZadSQLZad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Microsoft SQL Server");
+            Console.WriteLine(new string('-',25));
+            SqlConnection dbConn = new SqlConnection
+                (
+                  "Server =.;Database=master;Intergrated Security=true"
+
+                );
+            dbConn.Open();
+
+            using (dbConn)
+            {
+                SqlCommand command1 = new SqlCommand
+                    (
+                    "CREATE DATABASE Minions", dbConn
+                    );
+                command1.ExecuteNonQuery();
+            }
+        }
+    }
+}
